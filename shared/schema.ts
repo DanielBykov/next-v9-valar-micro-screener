1e07a463f5e2c0fafa787fafa91480e0f1d40c0d
Daniel 357 <3573053@gmail.com>
2026-03-15 14:29:28 +1300

    code formatting

--- shared/schema.ts
+++ shared/schema.ts
@@ -1,5 +1,4 @@
-import { sql } from "drizzle-orm";
-import { pgTable, text, varchar, integer, serial, date } from "drizzle-orm/pg-core";
+import { pgTable, text, integer, serial, date } from "drizzle-orm/pg-core";
 import { createInsertSchema } from "drizzle-zod";
 import { z } from "zod";
 
